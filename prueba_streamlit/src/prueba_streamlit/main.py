31347d1738bd50d7ecdf4a6bcc51d802b41d656d
abollit <abollit@minsait.com>
2024-12-10 13:34:59 +0100

    adssc

--- prueba_streamlit/src/prueba_streamlit/main.py
+++ prueba_streamlit/src/prueba_streamlit/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
 import os
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai_tools import DOCXSearchTool, MDXSearchTool
 
 # Asegúrate de agregar la ruta al directorio 'src'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
